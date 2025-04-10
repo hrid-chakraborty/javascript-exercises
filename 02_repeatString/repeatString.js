92d6ae4566a09f91052b40a3308e9ec8f6461332
Hrid Chakraborty <hridchakraborty@gmail.com>
2025-04-10 10:52:53 +0530

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(st, num) {
+    if(num < 0) {
+        return "ERROR"
+    }
+    let count = 0;
+    let res = "";
+    while(count < num) {
+        res += st;
+        count+=1;
+    }
+    return res;
 };
 
 // Do not edit below this line
