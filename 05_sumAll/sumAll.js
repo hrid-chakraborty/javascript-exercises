7b65d24c3b6dfb8ecce68f2f9f97fbe8d374b1b1
Hrid Chakraborty <hridchakraborty@gmail.com>
2025-04-10 11:36:48 +0530

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start != "number" || typeof end != "number" || start<0 || end<0) {
+        return "ERROR";
+    }
 
+    if(start%1!==0 || end%1!==0) {
+        return "ERROR";
+    }
+
+    if(start > end) {
+        [start, end] = [end, start];
+    }
+    let res = 0;
+    for(let i = start; i<=end; ++i) {
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
