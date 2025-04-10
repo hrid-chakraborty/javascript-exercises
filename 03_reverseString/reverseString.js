0d3ba558cd0bfdd4b0b185dea9acd98629b41f96
Hrid Chakraborty <hridchakraborty@gmail.com>
2025-04-10 11:01:36 +0530

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(st) {
+    let stArray = st.split("");
+    let left = 0;
+    let right = stArray.length - 1;
 
+    while(left<=right) {
+        [stArray[left], stArray[right]] = [stArray[right], stArray[left]];
+        left += 1;
+        right -= 1;
+    }
+
+    return stArray.join("");
 };
 
 // Do not edit below this line
