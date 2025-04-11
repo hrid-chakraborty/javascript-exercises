98e29052e69fad5d02146b074d9d127da27cbf64
Hrid Chakraborty <hridchakraborty@gmail.com>
2025-04-11 12:01:20 +0530

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+function isalnum(c) {
+    return /^[a-z0-9]+$/i.test(c);
+}
 
+const palindromes = function (word) {
+    let left = 0;
+    let right = word.length - 1;
+    while (left<right){
+        if(!isalnum(word[left])) {
+            left += 1;
+            continue;
+        }
+        if(!isalnum(word[right])) {
+            right -= 1;
+            continue;
+        }
+        if(word[left].toLowerCase()!=word[right].toLowerCase()) {
+            return false;
+        }
+        left += 1;
+        right -= 1;
+    }
+    return true;
 };
 
 // Do not edit below this line
