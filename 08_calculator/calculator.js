57a400d4208d129bee1b02e385a27a704dcf0d2a
Hrid Chakraborty <hridchakraborty@gmail.com>
2025-04-11 11:43:14 +0530

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, ele)=>(acc+ele),0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, ele)=>(acc*ele),1);
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  let result = 1;
+	for (let i = 0; i < p; ++i) {
+    result *= n;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  while(n > 0) {
+    result *= n;
+    n-=1;
+  }
+  return result;
 };
 
 // Do not edit below this line
