b9929f84c28e6e0c329dac826825f3efe1aa2e52
Hrid Chakraborty <hridchakraborty@gmail.com>
2025-04-11 12:16:08 +0530

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    if(typeof val == "string") {
+        val = parseInt(val);
+    }
+    if (val < 0) {
+        return "OOPS";
+    }
 
+    if (val == 0 || val == 1) {
+        return val;
+    }
+
+    let prev1 = 0;
+    let prev2 = 1;
+
+    for (let i  = 2; i <= val; i++){
+        let temp = prev2;
+        prev2 += prev1;
+        prev1 = temp; 
+    }
+
+    return prev2;
 };
 
 // Do not edit below this line
